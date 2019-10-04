4246e56ea217a3bd3bf6b94fc6f6e7f8b188315d
David Gómez Quilón <david.gomez.quilon@gmail.com>
2019-10-04 09:04:48 +0200

    [MIG] authenticator

--- authenticator/__manifest__.py
+++ authenticator/__manifest__.py
@@ -2,8 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": 'Authenticator',
-    "summary": 'Authentication from Aselcis Deployment Platform',
-    "version": '12.0.1.0',
+    "summary": 'Authentication from Asoo',
+    "version": '13.0.1.0',
     "depends": ['base'],
     "author": "Aselcis Consulting",
     "category": 'System',
